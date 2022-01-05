8194e8f9080315eda0ff19357c74696c21d61954
Alan Gutierrez <alanggdev87@outlook.com>
2022-01-04 21:52:47 -0600

    se sube proyecto boletines

---
+++ boletines/javascript/main.js
@@ -0,0 +1,42 @@
+//obtener los elementos d e la clase  .close
+//recorrerlos
+//agregar un evento click a cada uno de ellos
+
+
+let links = document.querySelectorAll(".close");
+
+links.forEach(function(link){ 
+
+    link.addEventListener("click",function(ev){
+        ev.preventDefault();
+
+        let content = document.querySelector('.content');
+
+        //quitar las clases de animacion que ya tiene
+        content.classList.remove("animate__pulse");
+        content.classList.remove("animate__animated");
+
+        //agregar clases para animar su salida
+        content.classList.add("animate__fadeOutUp");
+        content.classList.add("animate__animated");
+
+        setTimeout(function(){
+            location.href = "/boletines";
+
+        },600);
+       // setInterval(() => {
+            
+        //}, interval);
+                 
+
+    });
+
+});
+
+let iconos = document.querySelectorAll("i");
+
+iconos.forEach(function(icono){
+    icono.classList.remove("fa-star");
+
+
+})
